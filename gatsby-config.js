83f8984dc0f4cdf7685bdba63977080167a6fb30
Daniel Otieno <danielotieno.ke@gmail.com>
2020-12-19 13:25:35 +0300

    chore:setup the files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: "saas-website",
+    title: 'SAAS Charity Website',
+    description: 'This is a website SAAS Charity NGO from Mombasa, Kenya',
+    siteUrl: '#',
+    twitterUsername: '@ProGasKe',
+    keywords: ['Saas', 'Charity', 'Donate', 'Mombasa'],
   },
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        trackingId: "",
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
       },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+      __key: `images`,
     },
   ],
 };
