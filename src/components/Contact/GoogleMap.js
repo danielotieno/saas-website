2ebc7323abd72e757b694b8602f9f833bb9c6cd4
Daniel Otieno <danielotieno.ke@gmail.com>
2021-07-05 12:21:20 +0300

    chore:add new google map address

--- src/components/Contact/GoogleMap.js
+++ src/components/Contact/GoogleMap.js
@@ -4,8 +4,8 @@ const GoogleMap = ({ extraClass }) => {
   return (
     <div className={`google-map__${extraClass}`}>
       <iframe
-        title='saas google map'
-        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.8645736565445!2d39.68733151435208!3d-4.048039046013806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x184013cf5f7664a7%3A0x14b74ef130c74a6!2sSAAS!5e0!3m2!1sen!2ske!4v1612805963879!5m2!1sen!2ske'
+        title='SAAS Google Map'
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.3254138011253!2d-6.238924384297505!3d53.355330881739526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670ef5ced3db03%3A0x2833d255ddb0b632!2s24%20Fairfield%20Ave%2C%20East%20Wall%2C%20Dublin%2C%20D03%20TE27%2C%20Ireland!5e0!3m2!1sen!2ske!4v1625476716453!5m2!1sen!2ske'
         className={`map__${extraClass}`}
         allowFullScreen></iframe>
     </div>
--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import { Link } from 'gatsby';
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Link as ScrollLink } from 'react-scroll';
-import { Container, Row, Col } from 'react-bootstrap';
 import logoLight from '../../assets/images/saas.png';
 
 const Footer = () => {
@@ -25,24 +25,6 @@ const Footer = () => {
                   Number CHY 17318) that provides sustainable solutions to
                   communities in Kenya.
                 </p>
-                <ul className='list-unstyled footer-widget__contact'>
-                  <li>
-                    <a href='#none'>
-                      <i className='azino-icon-telephone'></i>+254 (041) 476396
-                    </a>
-                  </li>
-                  <li>
-                    <a href='#none'>
-                      <i className='azino-icon-email'></i>info@saas.ie
-                    </a>
-                  </li>
-                  <li>
-                    <a href='#none'>
-                      <i className='azino-icon-pin'></i>Umoja Road, Mombasa,
-                      Kenya
-                    </a>
-                  </li>
-                </ul>
               </div>
             </Col>
             <Col lg={4} md={6} sm={12}>
@@ -50,27 +32,22 @@ const Footer = () => {
             </Col>
             <Col lg={4} md={6} sm={12}>
               <div className='footer-widget mb-40 footer-widget__newsletter'>
-                <h3 className='footer-widget__title'>Newletter</h3>
-                <p>Signup now to get latest news & updates from us</p>
-                <form
-                  data-url='https://xyz.us18.list-manage.com/subscribe/post?u=20e91746ef818cd941998c598&id=cc0ee8140e'
-                  className='footer-widget__newsletter-form mc-form'>
-                  <label htmlFor='mc-email' className='sr-only'>
-                    Email Address
-                  </label>
-                  <input
-                    type='email'
-                    name='EMAIL'
-                    id='mc-email'
-                    className=''
-                    placeholder='Email address'
-                  />
-                  <div className='footer-widget__newsletter-btn-wrap d-flex justify-content-end'>
-                    <button type='submit' className='thm-btn '>
-                      Subscribe Now
-                    </button>
-                  </div>
-                </form>
+                <h3 className='footer-widget__title'>Contact Us</h3>
+                <ul className='list-unstyled footer-widget__contact'>
+                  <li>
+                    <i className='azino-icon-telephone'></i>+254 (041) 476396
+                  </li>
+                  <li>
+                    <i className='azino-icon-email'></i>info@saas.ie
+                  </li>
+                  <li>
+                    <i className='azino-icon-pin'></i>24 Fairfield Ave, East
+                    Wall, Dublin 3, D03 TE27, Ireland
+                  </li>
+                  <li>
+                    <i className='azino-icon-pin'></i>Umoja Road, Mombasa, Kenya
+                  </li>
+                </ul>
                 <div className='mc-form__response'></div>
               </div>
             </Col>
