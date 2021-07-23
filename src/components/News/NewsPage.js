281d792215558f78932e3fbb30af8f242dea4df4
Daniel Otieno <danielotieno.ke@gmail.com>
2021-07-23 14:56:26 +0300

    chore:refactor news page

--- src/components/News/NewsPage.js
+++ src/components/News/NewsPage.js
@@ -1,71 +1,26 @@
 import * as React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Pagination from '../../components/Pagination';
+import NEWS_DATA from '../../data/news';
 
-// markup
 const NewsPage = () => {
   return (
     <section className='news-details pt-80 pb-90'>
       <Container>
         <Row>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>Nov 2019 - March 2020</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>Aug 2019 - Nov 2019</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>April 2019 - July 2019</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>Dec 2018 - March 2019</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>Aug 2018 - Nov 2018</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
-          <Col md={12} lg={6}>
-            <div className='cause-details__presentations'>
-              <i className='fa fa-file-pdf'></i>
-              <h3>April 2018 - July 2018</h3>
-              <a href='#none' className='thm-btn dynamic-radius'>
-                Download
-              </a>
-            </div>
-          </Col>
+          {NEWS_DATA.map((data, index) => {
+            return (
+              <Col md={12} lg={6} key={index}>
+                <div className='cause-details__presentations'>
+                  <i className='fa fa-file-pdf'></i>
+                  <h3>{data.title}</h3>
+                  <a href={data.link} className='thm-btn dynamic-radius'>
+                    Download
+                  </a>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
         <Pagination />
       </Container>
---
+++ src/data/news.js
@@ -0,0 +1,40 @@
+import Apr18 from '../assets/images/news/Apr18.pdf';
+import Aug18 from '../assets/images/news/Aug18.pdf';
+import Dec20 from '../assets/images/news/Dec20.pdf';
+import July19 from '../assets/images/news/July19.pdf';
+import July20 from '../assets/images/news/July20.pdf';
+import March20 from '../assets/images/news/March20.pdf';
+import Nov19 from '../assets/images/news/Nov19.pdf';
+
+const NEWS_DATA = [
+  {
+    title: 'Aug 2020 - Dec 2020',
+    link: Dec20,
+  },
+  {
+    title: 'April 2020 - July 2020',
+    link: July20,
+  },
+  {
+    title: 'Nov 2019 - March 2020',
+    link: March20,
+  },
+  {
+    title: 'Aug 2019 - Nov 2019',
+    link: Nov19,
+  },
+  {
+    title: 'April 2019 - July 2019',
+    link: July19,
+  },
+  {
+    title: 'Aug 2018 - Nov 2018',
+    link: Aug18,
+  },
+  {
+    title: 'April 2018 - July 2018',
+    link: Apr18,
+  },
+];
+
+export default NEWS_DATA;
