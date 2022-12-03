102c448af33dfee059d7ca3990ed37712b454e89
Daniel Otieno <danielotieno.ke@gmail.com>
2022-12-03 10:53:28 +0300

    chore:add June - Oct newsletter

--- src/data/news.js
+++ src/data/news.js
@@ -10,8 +10,13 @@ import March20 from '../assets/images/news/March20.pdf';
 import Nov19 from '../assets/images/news/Nov19.pdf';
 import Sep21 from '../assets/images/news/Sep21.pdf';
 import Feb22 from '../assets/images/news/Feb22.pdf';
+import Jun22 from '../assets/images/news/June22.pdf';
 
 const NEWS_DATA = [
+  {
+    title: 'June 2022 - Oct 2022',
+    link: Jun22,
+  },
   {
     title: 'Feb 2022 - May 2022',
     link: Feb22,
