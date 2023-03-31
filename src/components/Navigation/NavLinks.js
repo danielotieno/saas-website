318e9dbff2c889b44ab12eef3678c93fb23a2af9
Daniel Otieno <danielotieno.ke@gmail.com>
2023-03-31 17:29:17 +0300

    fix link

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -21,12 +21,12 @@ const NavLinks = ({ extraClassName }) => {
         </button>
         <ul className='dropdown-list'>
           <li>
-            <Link to='/students/students-need-sponsorship'>
+            <Link to='/students/need-sponsorship'>
               Need Sponsorship
             </Link>
           </li>
           <li>
-            <Link to='/students/current-students'>Current Students</Link>
+            <Link to='/students/current'>Current Students</Link>
           </li>
         </ul>
       </li>
