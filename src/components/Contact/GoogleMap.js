df1259a3aeabcb651d9649e27883a916c3195fd7
Daniel Otieno <danielotieno.ke@gmail.com>
2023-07-17 12:18:14 +0300

    add mombasa office map

--- src/components/Contact/GoogleMap.js
+++ src/components/Contact/GoogleMap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const GoogleMap = ({ extraClass }) => {
+const GoogleMap = ({ extraClass, src }) => {
   return (
     <div className={`google-map__${extraClass}`}>
       <iframe
         title='SAAS Google Map'
-        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.3254138011253!2d-6.238924384297505!3d53.355330881739526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670ef5ced3db03%3A0x2833d255ddb0b632!2s24%20Fairfield%20Ave%2C%20East%20Wall%2C%20Dublin%2C%20D03%20TE27%2C%20Ireland!5e0!3m2!1sen!2ske!4v1625476716453!5m2!1sen!2ske'
+        src={`${src}`}
         className={`map__${extraClass}`}
         allowFullScreen></iframe>
     </div>
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -32,7 +32,14 @@ const Contact = () => {
         <StickyHeader extraClassName='stricky-header-two' />
         <PageHeader title='Contact Us' crumbTitle='Home' crumbLink='/' />
         <ContactForm />
-        <GoogleMap extraClass='contact' />
+        <GoogleMap
+          extraClass='contact'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.8638377584575!2d39.68709011185733!3d-4.04818874486821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x184013cf5f7664a7%3A0x14b74ef130c74a6!2sSponsor%20An%20Africa%20Scholar%20(SAAS)!5e0!3m2!1sen!2ske!4v1689585410801!5m2!1sen!2ske'
+        />
+        <GoogleMap
+          extraClass='contact'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.3254138011253!2d-6.238924384297505!3d53.355330881739526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670ef5ced3db03%3A0x2833d255ddb0b632!2s24%20Fairfield%20Ave%2C%20East%20Wall%2C%20Dublin%2C%20D03%20TE27%2C%20Ireland!5e0!3m2!1sen!2ske!4v1625476716453!5m2!1sen!2ske'
+        />
         <Footer />
       </Layout>
     </>
