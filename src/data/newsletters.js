5effd18163431fe42a30990985037aba87455fed
Daniel Otieno <danielotieno.ke@gmail.com>
2024-10-24 04:26:32 +0300

    added may 2024 newsletter

--- src/data/newsletters.js
+++ src/data/newsletters.js
@@ -1,88 +1,93 @@
-import Apr18 from "../assets/images/news/Apr18.pdf";
-import Apr21 from "../assets/images/news/Apr21.pdf";
-import Aug17 from "../assets/images/news/Aug17.pdf";
-import Aug18 from "../assets/images/news/Aug18.pdf";
-import Aug21 from "../assets/images/news/Aug21.pdf";
-import Dec20 from "../assets/images/news/Dec20.pdf";
-import July19 from "../assets/images/news/July19.pdf";
-import July20 from "../assets/images/news/July20.pdf";
-import March20 from "../assets/images/news/March20.pdf";
-import Nov19 from "../assets/images/news/Nov19.pdf";
-import Sep21 from "../assets/images/news/Sep21.pdf";
-import Feb22 from "../assets/images/news/Feb22.pdf";
-import Jun22 from "../assets/images/news/June22.pdf";
-import Nov22 from "../assets/images/news/Nov22.pdf";
-import Mar23 from "../assets/images/news/March23.pdf";
-import Aug23 from "../assets/images/news/Aug23.pdf";
-import Jan24 from "../assets/images/news/Jan24.pdf";
+import Apr18 from '../assets/images/news/Apr18.pdf';
+import Apr21 from '../assets/images/news/Apr21.pdf';
+import Aug17 from '../assets/images/news/Aug17.pdf';
+import Aug18 from '../assets/images/news/Aug18.pdf';
+import Aug21 from '../assets/images/news/Aug21.pdf';
+import Dec20 from '../assets/images/news/Dec20.pdf';
+import July19 from '../assets/images/news/July19.pdf';
+import July20 from '../assets/images/news/July20.pdf';
+import March20 from '../assets/images/news/March20.pdf';
+import Nov19 from '../assets/images/news/Nov19.pdf';
+import Sep21 from '../assets/images/news/Sep21.pdf';
+import Feb22 from '../assets/images/news/Feb22.pdf';
+import Jun22 from '../assets/images/news/June22.pdf';
+import Nov22 from '../assets/images/news/Nov22.pdf';
+import Mar23 from '../assets/images/news/March23.pdf';
+import Aug23 from '../assets/images/news/Aug23.pdf';
+import Jan24 from '../assets/images/news/Jan24.pdf';
+import May24 from '../assets/images/news/May24.pdf';
 
 const NEWS_LETTERS_DATA = [
   {
-    title: "Jan 2024 - April 2024",
+    title: 'May 2024 - Aug 2024',
+    link: May24,
+  },
+  {
+    title: 'Jan 2024 - April 2024',
     link: Jan24,
   },
   {
-    title: "Aug 2023 - Dec 2023",
+    title: 'Aug 2023 - Dec 2023',
     link: Aug23,
   },
   {
-    title: "March 2023 - Aug 2023",
+    title: 'March 2023 - Aug 2023',
     link: Mar23,
   },
   {
-    title: "Nov 2022 - Feb 2023",
+    title: 'Nov 2022 - Feb 2023',
     link: Nov22,
   },
   {
-    title: "June 2022 - Oct 2022",
+    title: 'June 2022 - Oct 2022',
     link: Jun22,
   },
   {
-    title: "Feb 2022 - May 2022",
+    title: 'Feb 2022 - May 2022',
     link: Feb22,
   },
   {
-    title: "Sep 2021 - Jan 2022",
+    title: 'Sep 2021 - Jan 2022',
     link: Sep21,
   },
   {
-    title: "May 2021 - Aug 2021",
+    title: 'May 2021 - Aug 2021',
     link: Aug21,
   },
   {
-    title: "Jan 2021 - April 2021",
+    title: 'Jan 2021 - April 2021',
     link: Apr21,
   },
   {
-    title: "Aug 2020 - Dec 2020",
+    title: 'Aug 2020 - Dec 2020',
     link: Dec20,
   },
   {
-    title: "April 2020 - July 2020",
+    title: 'April 2020 - July 2020',
     link: July20,
   },
   {
-    title: "Nov 2019 - March 2020",
+    title: 'Nov 2019 - March 2020',
     link: March20,
   },
   {
-    title: "Aug 2019 - Nov 2019",
+    title: 'Aug 2019 - Nov 2019',
     link: Nov19,
   },
   {
-    title: "April 2019 - July 2019",
+    title: 'April 2019 - July 2019',
     link: July19,
   },
   {
-    title: "Aug 2018 - Nov 2018",
+    title: 'Aug 2018 - Nov 2018',
     link: Aug18,
   },
   {
-    title: "April 2018 - July 2018",
+    title: 'April 2018 - July 2018',
     link: Apr18,
   },
   {
-    title: "Aug 2017 - Feb 2018",
+    title: 'Aug 2017 - Feb 2018',
     link: Aug17,
   },
 ];
