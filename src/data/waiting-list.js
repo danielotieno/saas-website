066a190883c870ff913495830a398bc733f77163
Daniel Otieno <danielotieno.ke@gmail.com>
2025-02-04 18:50:23 +0300

    added new waiting students

--- src/data/waiting-list.js
+++ src/data/waiting-list.js
@@ -1,50 +1,123 @@
-import George from '../assets/images/waiting/george.jpeg';
-import GeorgePdf from '../assets/images/waiting/george.pdf';
-import Helton from '../assets/images/waiting/helton.jpg';
-import HeltonPdf from '../assets/images/waiting/helton.pdf';
 import Alvin from '../assets/images/waiting/alvin.jpg';
 import AlvinPdf from '../assets/images/waiting/alvin.pdf';
-import Rukia from '../assets/images/waiting/rukia.jpeg';
-import RukiaPdf from '../assets/images/waiting/rukia.pdf';
 import Prilly from '../assets/images/waiting/prilly.jpeg';
 import PrillyPdf from '../assets/images/waiting/prilly.pdf';
+import Robert from '../assets/images/waiting/robert.jpeg';
+import RobertPdf from '../assets/images/waiting/robert.pdf';
+import Sheryl from '../assets/images/waiting/sheryl.jpeg';
+import SherylPdf from '../assets/images/waiting/sheryl.pdf';
+import Martin from '../assets/images/waiting/martin.jpeg';
+import MartinPdf from '../assets/images/waiting/martin.pdf';
+import Elizabeth from '../assets/images/waiting/elizabeth.jpeg';
+import ElizabethPdf from '../assets/images/waiting/elizabeth.pdf';
+import Maxwel from '../assets/images/waiting/maxwel.jpeg';
+import MaxwelPdf from '../assets/images/waiting/maxwel.pdf';
+import Nancy from '../assets/images/waiting/nancy.jpeg';
+import NancyPdf from '../assets/images/waiting/nancy.pdf';
+import Constance from '../assets/images/waiting/constance.jpeg';
+import ConstancePdf from '../assets/images/waiting/constance.pdf';
+import Omar from '../assets/images/waiting/omar.jpeg';
+import OmarPdf from '../assets/images/waiting/omar.pdf';
+import Riana from '../assets/images/waiting/riana.jpeg';
+import RianaPdf from '../assets/images/waiting/riana.pdf';
+import Rajab from '../assets/images/waiting/rajab.jpeg';
+import RajabPdf from '../assets/images/waiting/rajab.pdf';
 
 const WAITING_STUDENTS = [
   {
-    image: George,
-    title: 'George',
-    text: 'George is in form 3. He will be completing his secondary school education in the year 2023.',
-    link: GeorgePdf,
+    image: Alvin,
+    title: 'Alvin',
+    text: 'Alvin is 18 years old. His dream is to become a Doctor one day.',
+    link: AlvinPdf,
     isFeatured: true,
   },
 
   {
-    image: Helton,
-    title: 'Helton',
-    text: 'Helton is 15 years of age. He is in form 2 and a full-time border at Oriwo Boys High school.',
-    link: HeltonPdf,
-    isFeatured: true,
-  },
-  {
-    image: Alvin,
-    title: 'Alvin',
-    text: 'Alvin is 15 years of age. he is a very hardworking young boy. Currently, he is in form 1 and he will be completing her secondary level in 2025.',
-    link: AlvinPdf,
-    isFeatured: true,
-  },
-  {
-    image: Rukia,
-    title: 'Rukia',
-    text: 'Rukia is in form two at Butere Girls Secondary School and will complete her high school level in 2026',
-    link: RukiaPdf,
-    isFeatured: true,
-  },
-  {
     image: Prilly,
     title: 'Prilly',
-    text: 'Prilly is 14 years of age and the last born in a family of 3 children: two girls and a boy',
+    text: 'Prilly is 17 years old. She is currently in form 3 and dreams of becoming a Nurse one day.',
     link: PrillyPdf,
     isFeatured: true,
+  },
+
+  {
+    image: Robert,
+    title: 'Robert',
+    text: 'Robert is 18 years old and currently a Form 3 student. His dream is to become an Engineer one day.',
+    link: RobertPdf,
+    isFeatured: true,
+  },
+
+  {
+    image: Sheryl,
+    title: 'Sheryl',
+    text: 'Sheryl is 16 years old and currently in Form 3. Her dream is to become an Electrical Engineer one day.',
+    link: SherylPdf,
+    isFeatured: true,
+  },
+
+  {
+    image: Martin,
+    title: 'Martin',
+    text: 'Martin is 17 years old and currently in Form 2. His dream is to become a Doctor one day.',
+    link: MartinPdf,
+    isFeatured: true,
+  },
+
+  {
+    image: Elizabeth,
+    title: 'Elizabeth',
+    text: 'Elizabeth is 15 years old and currently in Form 3. Her dream is to become a Lawyer one day.',
+    link: ElizabethPdf,
+    isFeatured: true,
+  },
+
+  {
+    image: Maxwel,
+    title: 'Maxwel',
+    text: 'Maxwel is 16 years old. He dreams of becoming a Doctor one day.',
+    link: MaxwelPdf,
+    isFeatured: false,
+  },
+
+  {
+    image: Nancy,
+    title: 'Nancy',
+    text: 'Nancy is 17 years old and currently in form 3. She dreams of becoming a Nurse one day.',
+    link: NancyPdf,
+    isFeatured: false,
+  },
+
+  {
+    image: Constance,
+    title: 'Constance',
+    text: 'Constance is 16 years old. Her dream is to become a Lawyer one day.',
+    link: ConstancePdf,
+    isFeatured: false,
+  },
+
+  {
+    image: Omar,
+    title: 'Omar',
+    text: 'Omar is 18 years old and currently in form 3. He dreams of becoming a Doctor one day.',
+    link: OmarPdf,
+    isFeatured: false,
+  },
+
+  {
+    image: Riana,
+    title: 'Riana',
+    text: 'Riana is 16 years old. Her dream is to become a Pilot one day.',
+    link: RianaPdf,
+    isFeatured: false,
+  },
+
+  {
+    image: Rajab,
+    title: 'Rajab',
+    text: 'Rajab is 16 years old and currently in form 3. He dreams of becoming a Journalist one day.',
+    link: RajabPdf,
+    isFeatured: false,
   },
 ];
 
